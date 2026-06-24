e6acd300cbea41970edc745f39856eccbe61d8fe
Zack Bursk <zackbursk@gmail.com>
2026-06-23 22:33:30 -0600

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int i = 0; i < nums.length; i++){
+            if (seen.contains(nums[i])){
+                return true;
+            }
+            seen.add(nums[i]);
+        }
+        return false;
+    }
+}
